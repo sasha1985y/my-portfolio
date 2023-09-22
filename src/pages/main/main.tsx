67877de4463fca876d8f0494d1295abc9a51c8ab
sashaReact <kuyancev1985@mail.ru>
2023-09-22 19:53:37 +0300

    попробую изменить фото чёт не робит

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -3,8 +3,8 @@ import styles from './main.module.css';
 
 function Main(): JSX.Element {
   return (
-    <div className={styles.main_body_container}>
-      <div>
+    <div className={styles.grand_main_body_container}>
+      <div className={styles.main_body_container}>
         <div className={styles.main_container}>
           <h1>Главная страница всех портфолио</h1>
           <Link to="/cars">Cars</Link>;
