5193405323369bfd9e8cc5645b262ff1c5b52499
sashaReact <kuyancev1985@mail.ru>
2023-09-22 16:14:48 +0300

    попробую изменить фото чёт не робит

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,16 +1,11 @@
 import { Link } from 'react-router-dom';
-import styles from './main.module.css';
+//mport styles from './main.module.css';
 
 function Main(): JSX.Element {
   return (
     <>
-      <main className={styles.main_container}>
-        <aside>
-          <h1>Главная страница всех портфолио</h1>
-          <Link to="/cars">Cars</Link>;
-        </aside>
-        <img src="../public/img/обложка.png" alt="обложка" style={{height: '100vh', display: 'block', margin: '0 auto', opacity: '1'}}></img>
-      </main>
+      <h1>Главная страница всех портфолио</h1>
+      <Link to="/cars">Cars</Link>;
     </>
   );
 }
