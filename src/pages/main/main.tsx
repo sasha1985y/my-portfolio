2deae5abbff8a38b01f9bc8bb43079ba9ca674c5
sashaReact <kuyancev1985@mail.ru>
2023-09-22 19:26:52 +0300

    попробую изменить фото чёт не робит

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,15 +1,13 @@
 import { Link } from 'react-router-dom';
-//mport styles from './main.module.css';
+import styles from './main.module.css';
 
 function Main(): JSX.Element {
-  const styles = {
-    background: "url('../public/img/обложка.png') no-repeat center, url('https://media.giphy.com/media/ZwMF9XIF7eo9i/giphy.gif') no-repeat center fixed",
-    height: '100vh'
-  };
   return (
-    <div style={styles}>
-      <h1>Главная страница всех портфолио</h1>
-      <Link to="/cars">Cars</Link>;
+    <div className={styles.main_body_container}>
+      <div className={styles.main_container}>
+        <h1>Главная страница всех портфолио</h1>
+        <Link to="/cars">Cars</Link>;
+      </div>
     </div>
   );
 }
