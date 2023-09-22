7dc4af02c535acaefe0bba62dc0220577fb0430b
sashaReact <kuyancev1985@mail.ru>
2023-09-22 20:37:29 +0300

    попробую изменить фото чёт не робит

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
-import styles from './main.module.css';
+//import styles from './main.module.css';
 
 function Main(): JSX.Element {
   return (
-    <div className={styles.main_container}>
+    <>
       <h1>Главная страница всех портфолио</h1>
       <Link to="/cars">Cars</Link>;
-    </div>
+    </>
   );
 }
 
